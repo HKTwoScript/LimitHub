0a3746bb459d30bc55b5d760fd6783e1c1a4138a
HKTwoScript <jancmafs@gmail.com>
2025-08-24 12:07:30 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,2 @@
 ID="28e57f17-6fcc-4985-98be-c9d8ee33743d";
-loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
+loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
